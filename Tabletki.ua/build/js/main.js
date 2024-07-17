1332da5830ecbb064288b2e0870f818d9482d1c4
tutytut <tutytut@tutamail.com>
2024-07-17 11:41:25 +0300

    update translations

---
+++ Tabletki.ua/build/js/main.js
@@ -0,0 +1,1 @@
+$(document).ready((function(){$(".reserve__toggle-orders").on("click",(function(e){$("body").toggleClass("open")})),$(".header__return-reserve").on("click",(function(e){$("body").removeClass("open")}))}));
